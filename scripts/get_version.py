24777b373d1e9f4649522a74924ee6f512ae0024
Guillaume Gauvrit <guillaume@gauvr.it>
2024-10-30 20:56:56 +0100

    Get the version from the package

--- scripts/get_version.py
+++ scripts/get_version.py
@@ -1,7 +1,6 @@
-import importlib.metadata
+import pyramid_helloworld
 
-__version__ = importlib.metadata.version("pyramid-helloworld")
-
+__version__ = pyramid_helloworld.__version__
 
 if __name__ == "__main__":
     print(__version__, end="")
